50a4cfd191993a48ce286af44ca4e9860fe7ac65
Francescodario Cuzzocrea <bosconovic@gmail.com>
2025-06-18 08:34:33 +0200

    initial 22.2

--- picks.sh
+++ picks.sh
@@ -25,47 +25,47 @@ source build/envsetup.sh
 # device/xiaomi/sm8250-common
 
 # device/xiaomi/pipa
-#repopick 376507 376508 376509 376510 376513 376519 376520 376524 376525 376527 376529 376530 388725 398051 399010
+# repopick 376507 376508 376509 376510 376513 376519 376520 376524 376525 376527 376529 376530 388725 398051 399010
 
 # kernel/xiaomi/sm8250
-checkchain kernel/xiaomi/sm8250 refs/changes/83/388683/4
+# checkchain kernel/xiaomi/sm8250 refs/changes/83/388683/4
 
 # -------------- PLATFORM STUFF --------------
 
 # bootable/recovery
-repopick 398771 # minui: Import graphics_drm fix from QSSI 13
+# repopick 398771 # minui: Import graphics_drm fix from QSSI 13
 
 # build
-repopick -f 369796 # Sorry bro: 6 -> 3
+# repopick -f 369796 # Sorry bro: 6 -> 3
 
 # frameworks/base
-repopick -f -P frameworks/base 385457 # base: Initial SenseProvider for FaceSense service
-repopick -f -P frameworks/base 385463 # SystemUI: Implement face unlock recognition animation and text
-repopick -f -P frameworks/base 385464 # SystemUI: Use proper tint for face unlock icon
-repopick -f -P frameworks/base 391097 # SystemUI: biometrics: Follow user setting to confirm face unlock
-repopick -f -P frameworks/base 385512 # BiometricScheduler: Cancel operation if not idle
-repopick -f -P frameworks/base 397585 # base: Add three-fingers-swipe to screenshot [1/2]
-repopick -f -P frameworks/base 397586 # Improvements for swipe to screenshot
-repopick -f -P frameworks/base 397587 # SwipeToScreenshot: Import MIUI implementation
-repopick -f -P frameworks/base 369786 # Alter model name to avoid SafetyNet HW attestation enforcement
-repopick -f -P frameworks/base 369787 # keystore: Block key attestation for SafetyNet
-repopick -f -P frameworks/base 369788 # AttestationHooks: Set shipping level to 32 for devices >=33
-repopick -f -P frameworks/base 369789 # Limit SafetyNet workarounds to unstable GMS process
-repopick -f -P frameworks/base 369790 # gmscompat: Apply the SafetyNet workaround to Play Store aswell
-repopick -f -P frameworks/base 369791 # gmscompat: Use Nexus 6P fingerprint for CTS/Integrity
-repopick -f -P frameworks/base 369792 # gmscompat: Make CTS/Play Integrity pass again
-repopick -f -P frameworks/base 374392 # gmscompat: Use new info
+# repopick -f -P frameworks/base 385457 # base: Initial SenseProvider for FaceSense service
+# repopick -f -P frameworks/base 385463 # SystemUI: Implement face unlock recognition animation and text
+# repopick -f -P frameworks/base 385464 # SystemUI: Use proper tint for face unlock icon
+# repopick -f -P frameworks/base 391097 # SystemUI: biometrics: Follow user setting to confirm face unlock
+# repopick -f -P frameworks/base 385512 # BiometricScheduler: Cancel operation if not idle
+# repopick -f -P frameworks/base 397585 # base: Add three-fingers-swipe to screenshot [1/2]
+# repopick -f -P frameworks/base 397586 # Improvements for swipe to screenshot
+# repopick -f -P frameworks/base 397587 # SwipeToScreenshot: Import MIUI implementation
+# repopick -f -P frameworks/base 369786 # Alter model name to avoid SafetyNet HW attestation enforcement
+# repopick -f -P frameworks/base 369787 # keystore: Block key attestation for SafetyNet
+# repopick -f -P frameworks/base 369788 # AttestationHooks: Set shipping level to 32 for devices >=33
+# repopick -f -P frameworks/base 369789 # Limit SafetyNet workarounds to unstable GMS process
+# repopick -f -P frameworks/base 369790 # gmscompat: Apply the SafetyNet workaround to Play Store aswell
+# repopick -f -P frameworks/base 369791 # gmscompat: Use Nexus 6P fingerprint for CTS/Integrity
+# repopick -f -P frameworks/base 369792 # gmscompat: Make CTS/Play Integrity pass again
+# repopick -f -P frameworks/base 374392 # gmscompat: Use new info
 # repopick -f -P frameworks/base 357510 # gmscompat: also spoof props for samsung/sec apps
 
 # hardware/samsung_slsi-linaro/configs
 
 # hardware/samsung_slsi-linaro/exynos
-repopick 399491 # audiohal_comv1: return BT SCO device when an actual BT SCO device is used
-repopick 399492 # exynos: audiohal_comv1: Always allow to reconfigure stream if voice  recognition is active
-repopick 399493 # exynos: audiohal_comv1: Ensure AUDIO_INPUT_FLAG_FAST is set when using low latency capture samplerate during calls
+# repopick 399491 # audiohal_comv1: return BT SCO device when an actual BT SCO device is used
+# repopick 399492 # exynos: audiohal_comv1: Always allow to reconfigure stream if voice  recognition is active
+# repopick 399493 # exynos: audiohal_comv1: Ensure AUDIO_INPUT_FLAG_FAST is set when using low latency capture samplerate during calls
 
 # hardware/samsung_slsi-linaro/graphics
-repopick 384848 # WIP: libhwc2.1: invalidate last frame before changing display mode
+# repopick 384848 # WIP: libhwc2.1: invalidate last frame before changing display mode
 
 # hardware/samsung_slsi-linaro/interfaces
 
@@ -77,19 +77,19 @@ repopick 384848 # WIP: libhwc2.1: invalidate last frame before changing display 
 # repopick 357351 # hidl: fastcharge: add 1.1 impl
 
 # packages/apps/Settings
-repopick -f 385494 # Add Face Unlock with ParanoidSense
-repopick -f 385495 # Settings: Import missing Face Unlock animation
-repopick -f 391308 # Settings: Add three-fingers-swipe to screenshot [2/2]
+# repopick -f 385494 # Add Face Unlock with ParanoidSense
+# repopick -f 385495 # Settings: Import missing Face Unlock animation
+# repopick -f 391308 # Settings: Add three-fingers-swipe to screenshot [2/2]
 # repopick 357352 # Settings: add super fast charge toggle
 
 # packages/overlays/Lineage
-repopick -f 400129 # lineage: Moar fontz
+# repopick -f 400129 # lineage: Moar fontz
 
 # vendor/lineage
-repopick -f -P vendor/lineage 385496 # faceunlock
-repopick -f -P vendor/lineage 381475 # SystemUI: More notification icons on AOD screen
-repopick -f -P vendor/lineage 369794 # lineage: Disable privapp permission enforcement (make it log)
-repopick -f -P vendor/lineage 369795 # adb insecure by default
+# repopick -f -P vendor/lineage 385496 # faceunlock
+# repopick -f -P vendor/lineage 381475 # SystemUI: More notification icons on AOD screen
+# repopick -f -P vendor/lineage 369794 # lineage: Disable privapp permission enforcement (make it log)
+# repopick -f -P vendor/lineage 369795 # adb insecure by default
 # repopick 357968 # config: add super fast charge interface
 
 exit 0
