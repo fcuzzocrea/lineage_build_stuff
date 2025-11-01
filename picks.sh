bf719ef4301ae8e3ef63cefa7a51adbbad984e87
Francescodario Cuzzocrea <bosconovic@gmail.com>
2025-11-01 18:51:55 +0100

    update

--- picks.sh
+++ picks.sh
@@ -37,43 +37,43 @@ source build/envsetup.sh
 # build
 
 # frameworks/base
-repopick 458940 # SystemUI: Implement face unlock recognition animation and text
-repopick 458941 # SystemUI: Use proper tint for face unlock icon
-repopick 458942 # SystemUI: Implement bouncer face unlock animation
-repopick 458943 # SystemUI: Disable FaceUnlock Lockouts
-repopick 458944 # SenseProvider: Allow our face unlock to be used on third-party apps
-repopick 458945 # FaceUnlock: Remove background behind indicator on lockscreen
-repopick 458946 # SystemUI: Add face unlock icon to keyguard blueprint
-repopick 458947 # base: Initial SenseProvider for FaceSense service
-repopick 458948 # SystemUI: biometrics: Follow user setting to confirm face unlock
-repopick 458949 # FaceService: Conditionally add ParanoidSense
-repopick 458950 # FaceSense: Vibrate on successful authentication
-repopick 458951 # services: Force load FaceSense providers
-repopick 458952 # FaceUnlockImageView: Prevent statusBarStateController memory leak
-repopick 458953 # SystemUI: Fix face unlock indication static calls
-repopick 458954 # SenseProvider: Align with lineage
-repopick 458977 # base: Add three-fingers-swipe to screenshot [1/2]
-repopick 458983 # Alter model name to avoid SafetyNet HW attestation enforcement
-repopick 458984 # keystore: Block key attestation for SafetyNet
-repopick 458985 # AttestationHooks: Set shipping level to 32 for devices >=33
-repopick 458986 # Limit SafetyNet workarounds to unstable GMS process
-repopick 458987 # gmscompat: Apply the SafetyNet workaround to Play Store aswell
-repopick 458988 # gmscompat: Use Nexus 6P fingerprint for CTS/Integrity
-repopick 458989 # gmscompat: Make CTS/Play Integrity pass again
-repopick 458990 # gmscompat: Use new info
-repopick 458991 # gmscompat: Get package name from context instead of app
-repopick 458992 # gmscompat: Remove unwanted spoofing of DEVICE_INITIAL_SDK_INT
-repopick 458993 # gmscompat: Allow setting any field in certified build props
-repopick 458994 # gmscompat: Un-spoof GMS while adding google account
-repopick 458995 # gmscompat: refacor using switches
-repopick 458996 # gmscompat: also spoof props for samsung/sec apps
+repopick -f 458940 # SystemUI: Implement face unlock recognition animation and text
+repopick -f 458941 # SystemUI: Use proper tint for face unlock icon
+repopick -f 458942 # SystemUI: Implement bouncer face unlock animation
+repopick -f 458943 # SystemUI: Disable FaceUnlock Lockouts
+repopick -f 458944 # SenseProvider: Allow our face unlock to be used on third-party apps
+repopick -f 458945 # FaceUnlock: Remove background behind indicator on lockscreen
+repopick -f 458946 # SystemUI: Add face unlock icon to keyguard blueprint
+repopick -f 458947 # base: Initial SenseProvider for FaceSense service
+repopick -f 458948 # SystemUI: biometrics: Follow user setting to confirm face unlock
+repopick -f 458949 # FaceService: Conditionally add ParanoidSense
+repopick -f 458950 # FaceSense: Vibrate on successful authentication
+repopick -f 458951 # services: Force load FaceSense providers
+repopick -f 458952 # FaceUnlockImageView: Prevent statusBarStateController memory leak
+repopick -f 458953 # SystemUI: Fix face unlock indication static calls
+repopick -f 458954 # SenseProvider: Align with lineage
+repopick -f 458977 # base: Add three-fingers-swipe to screenshot [1/2]
+repopick -f 458983 # Alter model name to avoid SafetyNet HW attestation enforcement
+repopick -f 458984 # keystore: Block key attestation for SafetyNet
+repopick -f 458985 # AttestationHooks: Set shipping level to 32 for devices >=33
+repopick -f 458986 # Limit SafetyNet workarounds to unstable GMS process
+repopick -f 458987 # gmscompat: Apply the SafetyNet workaround to Play Store aswell
+repopick -f 458988 # gmscompat: Use Nexus 6P fingerprint for CTS/Integrity
+repopick -f 458989 # gmscompat: Make CTS/Play Integrity pass again
+repopick -f 458990 # gmscompat: Use new info
+repopick -f 458991 # gmscompat: Get package name from context instead of app
+repopick -f 458992 # gmscompat: Remove unwanted spoofing of DEVICE_INITIAL_SDK_INT
+repopick -f 458993 # gmscompat: Allow setting any field in certified build props
+repopick -f 458994 # gmscompat: Un-spoof GMS while adding google account
+repopick -f 458995 # gmscompat: refacor using switches
+repopick -f 458996 # gmscompat: also spoof props for samsung/sec apps
 
 # hardware/samsung_slsi-linaro/configs
 repopick 453260 # config: Check kernel version properly for openmax
 repopick 453261 # config: Initial configuration for s5e9945
 repopick 459041 # fixup! config: add openmax compatibility makefile
 repopick 459042 # openmax: wire up more configurables
-repopick 459043 # BoardConfig9945: rework 9945 configuration
+repopick -f 459043 # BoardConfig9945: rework 9945 configuration
 
 # hardware/samsung_slsi-linaro/exynos
 repopick 459044 # exynos: Add kernel-6.1-headers
@@ -100,26 +100,25 @@ repopick 456300 # Launcher3: Hide keyboard when transitioning to home screen
 repopick 456301 # Launcher3: Improve hidden & protected apps
 
 # packages/apps/Settings
-repopick 458955 # Add Face Unlock with ParanoidSense
-repopick 458956 # Settings: Import missing Face Unlock animation
-repopick 458957 # Settings: Fix crash with face lock preference for limited access users
-repopick 458958 # FaceEnroll: Fix circle camera view background color
-repopick 458959 # Settings: Create a ActivityDirector to optionally use SettingsGoogleFutureFaceEnroll
-repopick 458960 # Settings: Create face confirmation activity
-repopick 458961 # Settings: Update FaceEnroll ActivityDirector and Confirmation
-repopick 458962 # Settings: Use ActionPrimaryButton style for face buttons
-repopick 458978 # Settings: Add three-fingers-swipe to screenshot [2/2]
+repopick -f 458955 # Add Face Unlock with ParanoidSense
+repopick -f 458956 # Settings: Import missing Face Unlock animation
+repopick -f 458957 # Settings: Fix crash with face lock preference for limited access users
+repopick -f 458958 # FaceEnroll: Fix circle camera view background color
+repopick -f 458959 # Settings: Create a ActivityDirector to optionally use SettingsGoogleFutureFaceEnroll
+repopick -f 458960 # Settings: Create face confirmation activity
+repopick -f 458961 # Settings: Update FaceEnroll ActivityDirector and Confirmation
+repopick -f 458962 # Settings: Use ActionPrimaryButton style for face buttons
+repopick -f 458978 # Settings: Add three-fingers-swipe to screenshot [2/2]
 
 # packages/apps/SettingsIntelligence
 repopick 433482 # Style search bar to match new Settings UI
-repopick 452780 # Wip: Style search bar for 16
 
 # packages/overlays/Lineage
 
 # vendor/lineage
 repopick -f -P vendor/lineage 435923 # lineage: Disable privapp permission enforcement (make it log)
 repopick -f -P vendor/lineage 434170 # adb insecure by default
-repopick 458963 # lineage: Switch to AOSPA's face unlock implementation
+repopick -f 458963 # lineage: Switch to AOSPA's face unlock implementation
 
 # Permissive
 repopick -f 435920 # SELinux
